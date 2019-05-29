05902bc36ce97ec86032ed12e9b818e264f8435b
SwapnilOutscal <Swapnil@outscal.com>
2019-05-29 13:53:13 +0530

    Added - Occulsion Culling - Audio Optimization - Script Optimization

---
+++ Assets/Project/ScriptOptimization/OptimizedScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptimizedScript : MonoBehaviour
+{
+    void Update()
+    {
+        RotateGameObject();
+    }
+
+    public void RotateGameObject()
+    {
+        this.gameObject.transform.Rotate(0, 100 * Time.deltaTime, 0);
+        StartCoroutine(WaitRotate());
+    }
+
+    private IEnumerator WaitRotate()
+    {
+        yield return new WaitForSeconds(10);
+        this.gameObject.transform.Rotate(0, 100 * Time.deltaTime, 0);
+    }
+}
---
+++ Assets/Project/ScriptOptimization/Script.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script : MonoBehaviour
+{
+    void Update()
+    {
+        RotateGameObject();
+    }
+
+
+    public void RotateGameObject()
+    {
+        this.gameObject.transform.Rotate(0, 100 * Time.deltaTime, 0);
+
+        // use a for loop as a bad wait function
+
+        for (int i = 0; i < 1000000; i++) { }
+        this.gameObject.transform.Rotate(0, 100 * Time.deltaTime, 0);
+    }
+}
